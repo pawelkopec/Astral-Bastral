43caf64f5adf943181cdbc67548ad596ea19600f
pawelkopec <pawelkopec10@gmail.com>
2017-04-22 14:57:58 +0200

    Add test directory

---
+++ test/server/ClientConnectionManagerTest.java
@@ -0,0 +1,10 @@
+package server;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Paweł Kopeć on 22.04.17.
+ */
+class ClientConnectionManagerTest {
+
+}
---
+++ test/server/UDPAccessPointTest.java
@@ -0,0 +1,8 @@
+package server;
+
+/**
+ * Created by Paweł Kopeć on 22.04.17.
+ */
+class UDPAccessPointTest {
+
+}
