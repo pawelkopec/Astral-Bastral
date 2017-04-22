523be2116efc5a598d75a9253f34fc98b2a98810
pawelkopec <pawelkopec10@gmail.com>
2017-04-22 15:58:14 +0200

    Ports: add constant

--- src/server/Ports.java
+++ src/server/Ports.java
@@ -7,9 +7,9 @@ package server;
  */
 public class Ports {
 
-    static final int MIN_PORT_NUM = 2000, MAX_PORT_NUM = 65535;
+    static final int MIN_PORT_NUM = 2000, MAX_PORT_NUM = 65535, NO_PORT = -1;
 
-    static final String INVALID_PORT_NUMBER = "Port number must have a value between 0 and " + MAX_PORT_NUM + ".";
+    static final String INVALID_PORT_NUMBER = "Port number must have a value between " + MIN_PORT_NUM + " and " + MAX_PORT_NUM + ".";
 
     static boolean isValidPortNumber(int port) {
         return MIN_PORT_NUM <= port && port <= MAX_PORT_NUM;
