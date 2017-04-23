db920231fc40c054b953583be6cc026a10c7e5d1
pawelkopec <pawelkopec10@gmail.com>
2017-04-23 19:35:10 +0200

    Add ThreadTester

---
+++ test/testutil/ThreadTester.java
@@ -0,0 +1,33 @@
+package testutil;
+
+/**
+ * Created by vivace on 23.04.17.
+ *
+ * Class enabling catching asserion errors
+ * from another thread.
+ */
+public class ThreadTester {
+    private Thread thread;
+    private volatile AssertionError exc;
+
+    public ThreadTester(Runnable runnable) {
+        thread = new Thread(() -> {
+            try {
+                runnable.run();
+            } catch (AssertionError e) {
+                exc = e;
+            }
+        });
+    }
+
+    public void startThread() {
+        thread.start();
+    }
+
+    public void checkAsserion() throws InterruptedException {
+        thread.join();
+        if (exc != null) {
+            throw exc;
+        }
+    }
+}
