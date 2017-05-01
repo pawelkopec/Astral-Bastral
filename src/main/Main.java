be452a168f1dd402a83710a81190f377410b6166
pawelkopec <pawelkopec10@gmail.com>
2017-05-01 10:34:03 +0200

    Clean Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,25 +1,7 @@
 package main;
 
-import game.AstralBastralGame;
-import server.ClientConnectionManager;
-import server.PortManager;
-
-import java.io.IOException;
-import java.util.LinkedList;
-
 public class Main {
 
     public static void main(String[] args) {
-        LinkedList<Integer> ports = new LinkedList<>();
-        ports.add(9094);
-        ports.add(9091);
-        ports.add(9092);
-        ports.add(9093);
-        PortManager manager = new PortManager(ports, 4);
-        try {
-            new Thread(new ClientConnectionManager(new AstralBastralGame(), manager, 9090)).start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
